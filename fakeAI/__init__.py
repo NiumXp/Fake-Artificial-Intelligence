073d5cd9a71beb5603e8bcd0da1edda76b664582
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-07-01 17:15:02 -0300

    Update __init__.py

--- fakeAI/__init__.py
+++ fakeAI/__init__.py
@@ -2,13 +2,13 @@ from enum import IntEnum
 from typing import Tuple
 from collections.abc import Sequence
 
-numbers = (0, 1, 2)
+numbers = (0, 1, 2, 3)
 
 DEFAULT_CHARS = numbers
 
 
 class CellType(IntEnum):
-    NULL, WALL, MONSTER = numbers
+    NULL, WALL, MONSTER, EXIT = numbers
 
 
 class Monster:
@@ -26,12 +26,12 @@ class Monster:
     @property
     def neighbors(self) -> Tuple[CellType]:
         x, y = self._x, self._y
-        h, w = self._map.width - 1, self._map.height - 1
+        h, w = self._map.height - 1, self._map.width - 1
 
-        left  = self._map.cell((x - 1) if x > 0 else w, y)
-        top   = self._map.cell(x, y - (1 if y > 0 else h))
-        right = self._map.cell((x + 1) if x < w else w, y)
-        down  = self._map.cell(x, (y + 1) if y < 0 else h)
+        left  = self._map.cell(y, (x - 1) if x > 0 else x if x > w else w)
+        top   = self._map.cell((y - 1) if y > 0 else y if y > h else h, x)
+        right = self._map.cell(y, (x + 1) if x < w else 0 if x > 0 else w)
+        down  = self._map.cell((y + 1) if y < h else 0 if y > 0 else y, x)
 
         return [left, top, right, down]
 
@@ -75,20 +75,38 @@ class Monster:
 
 
 class Grid:
-    __slots__ = ("_inner", "_chars", "width", "height", "_function")
+    __slots__ = ("_inner", "_chars", "_w", "_h", "_function")
 
-    def __init__(self, width: int, height: int, *, characters: tuple=DEFAULT_CHARS):
-        self.width = width
-        self.height = height
-
+    def __init__(self, ls: list=None, *, characters: tuple=DEFAULT_CHARS):
         self._chars = characters
 
-        self._inner = [
-            [CellType.NULL for _ in range(width)]
-            for _ in range(height)
-        ]
+        if ls:
+            w = len(ls[0])
+            h = len(ls)
+
+            for line in ls:
+                if len(line) == w:
+                    for item in line:
+                        if type(item) is not CellType:
+                            raise TypeError("items of lines need to be CellType")
+                else:
+                    raise IndexError("lines need to has some width")
+        else:
+            w = h = 10
+            ls = self.null_list(w, h)
+
+        self._w, self._h = (w, h)
+        self._inner = ls
 
         self._function = lambda x, y, type: None
+
+    @property
+    def width(self) -> int:
+        return self._w
+
+    @property
+    def height(self) -> int:
+        return self._h
 
     @property
     def characters(self) -> tuple:
@@ -97,22 +115,39 @@ class Grid:
     @characters.setter
     def characters(self, new_value):
         if isinstance(new_value, Sequence):
-            if len(new_value) == 3:
+            if len(new_value) == 4:
                 self._chars = new_value
             else:
-                raise IndexError("sequence need has exactly 3 items")
+                raise IndexError("sequence need has exactly 4 items")
         else:
             raise TypeError("tuple expected")
 
-    def to_list(self):
-        return self._inner
+    @staticmethod
+    def null_list(self, width: int, height: int) -> list:
+        return [
+            [CellType.NULL for _ in range(width)]
+            for _ in range(height)
+        ]
+
+    def to_list(self, raw: bool=True) -> list:
+        if raw:
+            return self._inner
+        else:
+            return [map(lambda cell: self._chars[cell], line) for line in self._inner]
+
+    @classmethod
+    def from_list(cls, list_):
+        obj = cls(list_)
+        return obj
 
     def __str__(self):
-        return '\n'.join(' '.join(map(str, line)) for line in self.to_list())
+        grid = self.to_list(raw=False)
+        lines = [' '.join(map(str, line)) for line in grid]
+        return '\n'.join(lines)
 
-    def spawn_monster(self):
-        self.change_cell(0, 0, CellType.MONSTER)
-        return Monster((0, 0), self)
+    def spawn_monster(self, x: int, y: int) -> Monster:
+        self.change_cell(x, y, CellType.MONSTER)
+        return Monster((x, y), self)
 
     def change_cell(self, column, line, celltype: CellType):
         if celltype in CellType:
@@ -120,5 +155,5 @@ class Grid:
         else:
             raise TypeError("'CellType' expected")
 
-    def cell(self, column, line):
-        return self._inner[column][line]
+    def cell(self, row, column):
+        return self._inner[row][column]
