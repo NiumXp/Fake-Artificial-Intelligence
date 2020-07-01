a7780db91f8d511cc3f3bc7f4afb579467f0bc81
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-07-01 15:57:29 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="fake-artificial-intelligence",
+      author="NiumXp",
+      url="https://github.com/NiumXp/Fake-Artificial-Intelligence",
+      version="1.0.0",
+      packages=["fakeAI"],
+      license="MIT",
+      description="A module to create fake artificial intelligence!"
+      python_requires=">=3.8"
+)
