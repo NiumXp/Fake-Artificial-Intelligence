0ebd38cef23602b2ac09ba95cf2745e361a928ca
NiumXp <43611165+NiumXp@users.noreply.github.com>
2020-07-01 19:18:11 -0300

    Update terminal.py

--- fakeAI/examples/terminal.py
+++ fakeAI/examples/terminal.py
@@ -4,7 +4,7 @@ from random import choice
 
 from fakeAI import Grid, CellType
 
-NULL, WALL = CellType.NULL, CellType.WALL
+NULL, WALL, EXIT = CellType.NULL, CellType.WALL, CellType.EXIT
 my_map = [
     [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL],
     [WALL, NULL, WALL, NULL, NULL, WALL, NULL, NULL, WALL],
@@ -14,29 +14,66 @@ my_map = [
     [WALL, NULL, NULL, NULL, WALL, NULL, WALL, WALL, WALL],
     [WALL, NULL, WALL, NULL, NULL, NULL, WALL, NULL, WALL],
     [WALL, NULL, NULL, NULL, WALL, NULL, NULL, NULL, WALL],
-    [WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL],
+    [WALL, EXIT, WALL, WALL, WALL, WALL, WALL, WALL, WALL],
 ]
 
-grid = Grid.from_list(my_map)
-grid.characters = (' ', '#', 'O', '\033[1;31mX\033[0m')
+
+class MyGrid(Grid):
+    def render(self, line: list):
+        transformed_line = [self.char(cell) for cell in line]
+
+        wall_color, end = "\033[1;31m", "\033[0m"
+
+        if CellType.EXIT not in line and CellType.MONSTER not in line:
+            line = [wall_color + ' '.join(transformed_line) + end]
+        else:
+            if CellType.MONSTER in line:
+                monster_pos = line.index(CellType.MONSTER)
+
+                leftf = "{}{}{}".format(wall_color, ' '.join(transformed_line[:monster_pos]), end)
+                rightf = "{}{}".format(wall_color, ' '.join(transformed_line[monster_pos + 1:]))
+
+                line = [leftf, "\033[1;32m" + self.char(CellType.MONSTER) + end, rightf + end]
+
+            if CellType.EXIT in line:
+                exit_pos = line.index(CellType.EXIT)
+
+                leftf = "{}{}{}".format(wall_color, ' '.join(transformed_line[:exit_pos]), end)
+                rightf = "{}{}{}".format(wall_color, ' '.join(transformed_line[exit_pos + 1:]), end)
+
+                line = [leftf, "\033[1;33m" + self.char(CellType.EXIT) + end, rightf]
+
+        return ' '.join(line)
+
+
+grid = MyGrid.from_list(my_map)
+grid.characters = (' ', '#', 'O', 'X')
+
 monster = grid.spawn_monster(1, 1)
 
-while True:
-    system("cls")
-    
-    neighbors = monster.neighbors
-    print(neighbors)
-    move_functions = monster.move_functions
+move_functions = monster.move_functions
 
+monster_in_maze = True
+while monster_in_maze:
+    system("cls")
     print(grid)
 
-    directions = []
-    for i, v in enumerate(neighbors):
-        if v == CellType.NULL:
-            directions.append(move_functions[i])
-    print(directions)
-    direction = choice(directions)
+    neighbors = monster.neighbors
+
+    try:
+        pos = neighbors.index(CellType.EXIT)
+    except ValueError:
+        directions = [move_functions[i]
+                    for i, v in enumerate(neighbors)
+                    if v == CellType.NULL]
+
+        direction = choice(directions)
+    else:
+        direction = move_functions[pos]
+        monster_in_maze = False
 
     direction()
 
-    sleep(1/4)
+    sleep(1/6)
+
+print("O monstrinho encontrou a saída!")
